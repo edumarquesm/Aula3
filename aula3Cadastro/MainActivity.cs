b39a185852be41335dfb8b9be8a286c9892fc686
Eduardo Molena <eduardo@matheussolucoes.com>
2017-12-21 14:11:17 -0200

    Tela de Cadastro
    -Uso de Navigation e Constructor.

---
+++ aula3Cadastro/MainActivity.cs
@@ -0,0 +1,34 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Content;
+
+namespace aula3Cadastro
+{
+    [Activity(Label = "aula3Cadastro", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            var nome = FindViewById<EditText>(Resource.Id.nome);
+            var sobrenome = FindViewById<EditText>(Resource.Id.sobrenome);
+            var dta_nasc = FindViewById<EditText>(Resource.Id.dta_nascimento);
+            var btn = FindViewById<Button>(Resource.Id.Enviar);
+
+            btn.Click += delegate {
+                var intencao = new Intent(this,typeof(SecondActivity));
+
+                intencao.PutExtra("nome", nome.Text);
+                intencao.PutExtra("sobrenome", sobrenome.Text);
+                intencao.PutExtra("dta_nasc", dta_nasc.Text);
+
+                StartActivity(intencao);
+            };
+        }
+    }
+}
+
---
+++ aula3Cadastro/SecondActivity.cs
@@ -0,0 +1,44 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace aula3Cadastro
+{
+    [Activity(Label = "SecondActivity")]
+    public class SecondActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Recebe);
+
+            var nome = FindViewById<TextView>(Resource.Id.nome);
+            var sobrenome = FindViewById<TextView>(Resource.Id.sobrenome);
+            var dta_nasc = FindViewById<TextView>(Resource.Id.dta_nascimento);
+            var btnV = FindViewById<Button>(Resource.Id.Voltar);
+
+
+            nome.Text = (String) Intent.GetStringExtra("nome");
+            sobrenome.Text = (String) Intent.GetStringExtra("sobrenome");
+            dta_nasc.Text = (String) Intent.GetStringExtra("dta_nasc");
+        
+            btnV.Click += delegate {
+
+                var intencao = new Intent(this, typeof(MainActivity));
+                
+                StartActivity(intencao);
+            };
+
+        }
+    }
+}
